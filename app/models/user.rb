12c8be9d03ce784c0db596d7cee62188245373a8
haze-yuuji <hazehaze1982@yahoo.co.jp>
2021-03-27 12:01:13 +0900

    パスワード　半角英数字　バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+         validates :password, format: { with: VALID_PASSWORD_REGEX }
+         
+         
          validates :nickname, presence: true
          validates :first_name, presence: true
          validates :last_name, presence: true
          validates :first_name_kana, presence: true
          validates :last_name_kana, presence: true
          validates :birthday, presence: true
+
+         
 
 
 end
