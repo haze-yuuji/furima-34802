3ea50039ade9240ee28ced15603f98f410c7e25d
haze-yuuji <hazehaze1982@yahoo.co.jp>
2021-03-27 12:44:39 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-         validates :password, format: { with: VALID_PASSWORD_REGEX }
-         
-         
-         validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
-         validates :birthday, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-         
-
-
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ一]+\z/ }
+  validates :birthday, presence: true
 end
