7e1aa026eb27ec81dc62db310ef8b78fa2f8e6c3
haze-yuuji <hazehaze1982@yahoo.co.jp>
2021-04-03 11:17:45 +0900

    再、last_nameが名字、first_nameが名前、コード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email                 { 'test@example' }
     password              { 'hhh000' }
     password_confirmation { password }
-    first_name            { '櫨' }
-    last_name             { 'ユウじ' }
-    first_name_kana       { 'ハゼ' }
-    last_name_kana        { 'ユウジ' }
+    first_name            { 'ユウじ' }
+    last_name             { '櫨' }
+    first_name_kana       { 'ユウジ' }
+    last_name_kana        { 'ハゼ' }
     birthday              { '1982-08-22' }
   end
 end
